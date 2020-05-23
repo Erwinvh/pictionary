7c38b80edce0a383e95df09fc3ed11b34c06851e
Erwin <Erwinvanhunnik@gmail.com>
2020-05-23 13:29:29 +0200

    Auto stash before merge of "developement" and "origin/developement"

--- src/Windows/HomeWindow.java
+++ src/Windows/HomeWindow.java
@@ -1,8 +1,44 @@
 package windows;
 
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.control.*;
+import javafx.scene.layout.VBox;
+
 public class HomeWindow {
 
+    public HomeWindow() {
+        VBox base = new VBox();
+        base.getChildren().add(new Label("Pictionary"));
+        base.getChildren().add(getPlayerInformation());
+        base.getChildren().add(getJoinHostButtons());
 
+
+
+
+
+    }
+
+
+    public HBox getPlayerInformation(){
+        HBox PlayerInfo = new HBox();
+        TextField username = new TextField();
+        Button LeftButton = new Button();
+        ImageView profileImage = new ImageView();
+        Button RightButton = new Button();
+        PlayerInfo.getChildren().addAll(username,LeftButton,profileImage,RightButton);
+        PlayerInfo.setSpacing(10);
+        return PlayerInfo;
+    }
+
+    public GridPane getJoinHostButtons(){
+        GridPane joinHostButtons = new GridPane();
+
+
+
+        return joinHostButtons;
+    }
 
 
 
