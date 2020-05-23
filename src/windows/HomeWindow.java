e41416bd01d49443d6604cee774c6a074ae78f5b
Erwin <Erwinvanhunnik@gmail.com>
2020-05-23 22:18:31 +0200

    [Addition] user picture added and rotation buttons work

--- src/windows/HomeWindow.java
+++ src/windows/HomeWindow.java
@@ -1,38 +1,76 @@
 package windows;
 
+import comms.Client;
+import comms.Message;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.control.*;
 import javafx.scene.layout.VBox;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class HomeWindow {
 
     private Scene homeWindowScene;
+    private int pictureIndex = 0;
+    private ArrayList<String> pictureList = new ArrayList<>();
+    private ImageView profileImage = new ImageView();
 
     public HomeWindow() {
+        List<String> namesList = Arrays.asList( "cat", "chicken", "chip", "dog", "donkey", "goldy", "owl", "pengiun", "pine", "raccoon", "robot", "rudolph", "sticktail", "union", "vampier");
+        pictureList.addAll(namesList);
         VBox base = new VBox();
-        base.getChildren().add(new Label("Pictionary"));
-        base.getChildren().add(getPlayerInformation());
-        base.getChildren().add(getJoinHostButtons());
+        base.getChildren().addAll(new Label("Pictionary"),getPlayerInformation(), getJoinHostButtons());
         base.setAlignment(Pos.CENTER);
         homeWindowScene = new Scene(base);
-
     }
 
 
     public HBox getPlayerInformation(){
         HBox playerInfo = new HBox();
         TextField username = new TextField();
+
+//        ImageView profileImage = new ImageView();
         Button leftButton = new Button("<-");
-        ImageView profileImage = new ImageView();
+        leftButton.setOnAction(event -> {
+            if (pictureIndex>0) {
+                pictureIndex--;
+            }else{
+                pictureIndex=14;
+            }
+setImageView();
+        });
+        File file = new File("resources/pictures/cat.jpg");
+        profileImage.setImage(new Image(file.toURI().toString()));
         Button rightButton = new Button("->");
+        rightButton.setOnAction(event -> {
+            if (pictureIndex<=13) {
+                pictureIndex++;
+            }else{
+                pictureIndex=0;
+            }
+setImageView();
+        });
+        profileImage.setFitHeight(120);
+        profileImage.setFitWidth(120);
+
         playerInfo.getChildren().addAll(username,leftButton,profileImage,rightButton);
         playerInfo.setSpacing(10);
         playerInfo.setAlignment(Pos.CENTER);
         return playerInfo;
+    }
+
+    public void setImageView(){
+        String fileName = "resources/pictures/"+pictureList.get(pictureIndex)+".jpg";
+        File newFile = new File(fileName);
+        profileImage.setImage(new Image(newFile.toURI().toString()));
     }
 
     public GridPane getJoinHostButtons(){
