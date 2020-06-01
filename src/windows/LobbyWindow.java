3e2587a320280c2f6e5c19cb164b12729c4d3942
Arne de Beer <arnedebeer2001@gmail.com>
2020-06-01 13:15:39 +0200

    [Change] ServerSettings in LobbyWindow are now greyed out when not host

--- src/windows/LobbyWindow.java
+++ src/windows/LobbyWindow.java
@@ -43,6 +43,7 @@ public class LobbyWindow {
 
     private VBox getGameSettingsBox() {
         VBox gameSettingsBox = new VBox();
+        gameSettingsBox.setDisable(!Client.getInstance().getUser().isHost());
         gameSettingsBox.setAlignment(Pos.CENTER_LEFT);
         gameSettingsBox.setSpacing(10);
 
